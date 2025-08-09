38a1de3ad7853ade9f5eb6870d29a758ad35c106
JS MacBook Pro <j_seok@JSui-MacBookPro.local>
2025-08-09 10:46:50 +0900

    날짜 UI 추가 및 버튼 스타일 추가

---
+++ src/todayDate.jsx
@@ -0,0 +1,31 @@
+import styles from './todayDate.module.css'
+
+function TodayDate(){
+    const today = new Date();
+
+    // 숫자 일 (day)
+    const day = String(today.getDate()).padStart(2,'0');
+
+    // const month = String(today.getMonth()+1).padStart(2,'0');
+
+    const monthYear = today.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short', // Jan, Feb, Mar, ..., Dec
+  }); 
+
+    const weekdays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+    const weekday = weekdays[today.getDay()];
+
+    return (
+        
+        <div className={styles.container}>
+            <div className={styles.day}>{day}</div>
+            <div className={styles.monthWeekday}>
+                <div className={styles.month}>{monthYear}</div>
+                <div className={styles.weekday}>{weekday}</div>
+            </div>
+        </div>
+    );
+}
+
+export default TodayDate;
