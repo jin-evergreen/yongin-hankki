aa1e2f73cb8eadff21aeca8897ee38656a9b43e9
JS MacBook Pro <j_seokpark@naver.com>
2025-08-21 20:20:15 +0900

    Favicon 설정 및 제휴 식당 URL 변경

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import RestaurantSelection from './Components/Restaurant-selection'
 import Calendar from './Components/Calendar'
 import MenuDisplay from './Components/MenuDisplay'
 
-function App() {
+function App() { 
 
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -21,7 +21,7 @@ function App() {
           <div className='date-button-wrapper'>
             <TodayDate />
             <button className="coalition-button" 
-            onClick={()=>window.open('https://map.naver.com/p/favorite/myPlace/folder/841c9b6f17c4413eb548bd090aa60fcd/pc?c=14.00,0,0,0,dh','_blank')}>
+            onClick={()=>window.open('https://map.naver.com/p/favorite/myPlace/folder/841c9b6f17c4413eb548bd090aa60fcd/pc?c=9.00,0,0,0,dh','_blank')}>
               제휴 식당 보기</button>
           </div>
           < RestaurantSelection 
